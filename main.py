3eedff55c0e7ffbed7e8ae01aa4684b5126fa65f
Rawwl03 <100472372@alumnos.uc3m.es>
2023-12-06 19:44:51 +0100

    alguien sion top?

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import csv, sys, constraint
 
+"""VARIABLES GLOBALES"""
+filas_problema = 0
+columnas_problema = 0
 
 def obtener_struct(filaxcol):
     fxc = filaxcol[0].split('x')
@@ -24,25 +27,36 @@ def crear_dominio(filas,columnas):
             dominio.append((i,j))
     return dominio
 
-def crear_variables(problem,variablesTNU,variablesTSU,plazas_electricas,variablesCongelador,filas,columnas):
-    dominio=crear_dominio(filas,columnas)
+def crear_variables(problem,variablesTNU,variablesTSU,plazas_electricas,variablesCongelador):
+    global filas_problema, columnas_problema
+    dominio=crear_dominio(filas_problema,columnas_problema)
     problem.addVariables(variablesCongelador,plazas_electricas)
     variablesTNU_sincongelador= list(set(variablesTNU)- set(variablesCongelador))
     variablesTSU_sincongelador = list(set(variablesTSU) - set(variablesCongelador))
     problem.addVariables(variablesTNU_sincongelador,dominio)
     problem.addVariables(variablesTSU_sincongelador, dominio)
 
+def crear_restricciones(problem, variablesTNU, variablesTSU):
+    variables = variablesTSU + variablesTNU
+    problem.addConstraint(notSamePlace, variables)
+    problem.addConstraint(noGatoEncerrado, variables)
+    for var_TSU in variablesTSU:
+        problem.addConstraint(notTNUinfront, [var_TSU] + variablesTNU)
 
 def ejecucion():
+    global filas_problema, columnas_problema
     datos = lectura()
     filas, columnas = obtener_struct(datos)
+    filas_problema = filas
+    columnas_problema = columnas
     plazas_electricidad = obtenerPE(datos[1])
     variablesTNU, variablesTSU, variablesCongelador=obtenerVars(datos)
     #creación problema
     problem = constraint.Problem()
-    crear_variables(problem,variablesTNU,variablesTSU,plazas_electricidad,variablesCongelador,filas,columnas)
-    for variable, dominio in problem._variables.items():
-        print(f"{variable}: {dominio}")
+    crear_variables(problem,variablesTNU,variablesTSU,plazas_electricidad,variablesCongelador)
+    crear_restricciones(problem, variablesTNU, variablesTSU)
+    solutions = problem.getSolutions()
+    print(solutions)
 
 def notSamePlace(*args):
     for i in range(0, len(args)):
@@ -53,12 +67,32 @@ def notSamePlace(*args):
 
 def notTNUinfront(*args):
     for i in range(1, len(args)):
-        if args[i][0] == args[0][0] and args[i][1] > args[0][0]:
+        if args[i][0] == args[0][0] and args[i][1] > args[0][1]:
             return False
     return True
 
+def noGatoEncerrado(*args):
+    global filas_problema
+    for i in range(0, len(args)):
+        izda = False
+        dcha = False
+        for j in range(i + 1, len(args)):
+            if args[i][0] != 1 and args[i][0] != filas_problema:
+                if i!=j and args[i][0] == args[j][0]+1 and args[i][1] == args[j][1]:
+                    izda = True
+                elif i!=j and args[i][0] == args[j][0]-1 and args[i][1] == args[j][1]:
+                    dcha = True
+                if dcha and izda:
+                    return False
+            elif args[i][0] == 1:
+                if i!=j and args[j][0] == 2 and args[j][1] == args[i][1]:
+                    return False
+            elif args[i][0] == filas_problema:
+                if i!=j and args[j][0] == filas_problema-1 and args[j][1] == args[i][1]:
+                    return False
+    return True
+
 def obtenerVars(datos):
-    variables = []
     variablesTNU = []
     variablesTSU = []
     varCongelador = []
