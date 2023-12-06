4c0994c3d33fa46da5e43429f2acb302e525f456
Rawwl03 <100472372@alumnos.uc3m.es>
2023-12-06 17:44:13 +0100

    masoneria

--- main.py
+++ main.py
@@ -24,10 +24,38 @@ def ejecucion():
     datos = lectura()
     filas, columnas = obtener_struct(datos[0])
     plazas_electricidad = datos[1]
+    indexTNU, indexTSU, variables =obtenerVars(datos)
     #creación problema
     problem = constraint.Problem()
 
-def notSamePlace(a, b):
+def notSamePlace(*args):
+    for i in range(0, len(args)):
+        for j in range(i+1, len(args)):
+            if i!=j and args[i] == args[j]:
+                return False
+    return True
+
+def notTNUinfront(*args):
+    for i in range(1, len(args)):
+        if args[i][0] == args[0][0] and args[i][1] > args[0][0]:
+            return False
+    return True
+
+def obtenerVars(datos):
+    variables = []
+    indexTNU = []
+    indexTSU = []
+    for i in range(2, datos):
+        data = datos[i].split("-")
+        if data[1] == "TSU":
+            indexTSU.append(i-2)
+        elif data[1] == "TNU":
+            indexTNU.append(i-2)
+        variables.append(datos[i])
+    return indexTNU, indexTSU, variables
+
+def obtenerPE(datos):
+
 
 
 if __name__ == "__main__":
