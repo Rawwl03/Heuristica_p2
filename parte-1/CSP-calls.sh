751ecd8af727d0e7a4be3a13f57a997c3d5cdf05
Rawwl03 <100472372@alumnos.uc3m.es>
2023-12-15 23:01:03 +0100

    3 pitones

--- parte-1/CSP-calls.sh
+++ parte-1/CSP-calls.sh
@@ -1,8 +1,8 @@
 !#/bin/bash
 
-python CSPParking.py ./CSP-tests/parking1.txt
-python CSPParking.py ./CSP-tests/parking2.txt
-python CSPParking.py ./CSP-tests/parking3.txt
-python CSPParking.py ./CSP-tests/parking4.txt
-python CSPParking.py ./CSP-tests/parking5.txt
-python CSPParking.py ./CSP-tests/parking6.txt
+python3 CSPParking.py ./CSP-tests/parking1.txt
+python3 CSPParking.py ./CSP-tests/parking2.txt
+python3 CSPParking.py ./CSP-tests/parking3.txt
+python3 CSPParking.py ./CSP-tests/parking4.txt
+python3 CSPParking.py ./CSP-tests/parking5.txt
+python3 CSPParking.py ./CSP-tests/parking6.txt
